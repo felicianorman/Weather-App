05f45f64fcc57f406541a8ed29abb561ebc320af
Felicia <feelicianorman@hotmail.com>
2023-03-19 18:32:19 +0100

    cors

--- src/ts/services/api.ts
+++ src/ts/services/api.ts
@@ -3,11 +3,9 @@ import { IWeatherResponse } from "../models/IWeatherResponse";
 // import { ILocation } from "../models/ILocation";
 
 export function getWeather(searchText: string): Promise<IWeatherResponse[]> {
-  const weatherAPI = `/.netlify/functions/location?key=4da078cc9068479c9ef140703231903&q=${
-  searchText}&lang=sv`
   return axios
     .get(
-      "https://api.weatherapi.com/v1/current.json?key=4da078cc9068479c9ef140703231903&q=" +
+      "https://cors-anywhere.herokuapp.com/https://api.weatherapi.com/v1/current.json?key=4da078cc9068479c9ef140703231903&q=" +
         searchText +
         "&lang=sv"
     )
