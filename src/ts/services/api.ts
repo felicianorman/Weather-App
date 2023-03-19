08eb3b4840f2d612af506cfc4ac06c6d8937e63d
Felicia <feelicianorman@hotmail.com>
2023-03-19 17:07:10 +0100

    vercel

--- src/ts/services/api.ts
+++ src/ts/services/api.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 import { IWeatherResponse } from "../models/IWeatherResponse";
 // import { ILocation } from "../models/ILocation";
 
-export function getWeather(searchText: HTMLInputElement): Promise<IWeatherResponse[]> {
+export function getWeather(searchText: string): Promise<IWeatherResponse[]> {
+  const weatherAPI = `/.netlify/functions/location?key=4da078cc9068479c9ef140703231903&q=${
+  searchText}&lang=sv`
   return axios
     .get(
       "http://api.weatherapi.com/v1/current.json?key=4da078cc9068479c9ef140703231903&q=" +
